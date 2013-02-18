8f679a163ff5546b28bf6072788182d7b41984e4
Jonathan Aquino <jon@ning.com>
2013-02-18 10:25:28 -0800

    Deleted all Rails files, in preparation for rewriting Yubnub in PHP

--- app/models/banned_url_pattern.rb
+++
@@ -1,2 +0,0 @@
-class BannedUrlPattern < ActiveRecord::Base
-end
--- config/environments/production.rb
+++
@@ -1,5 +0,0 @@
-Dependencies.mechanism                             = :require
-ActionController::Base.consider_all_requests_local = true
-ActionController::Base.perform_caching             = true
-
-
