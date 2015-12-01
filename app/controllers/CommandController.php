fa7c9244ca057b8d1ea45c6f33ac6f6fbfb610f2
Jonathan Aquino <jonathan.aquino@gmail.com>
2015-11-30 20:03:54 -0800

    Upgrade to new ReCaptcha.

--- app/controllers/CommandController.php
+++ app/controllers/CommandController.php
@@ -32,13 +32,13 @@ class CommandController extends Controller {
      * @param string $errorMessage  an error message if the submission failed
      */
     public function action_new($errorMessage = null) {
-        require_once SERVER_ROOT . '/lib/Recaptcha/recaptchalib.php';
         $this->render('new', array(
             'pageTitle' => 'Create A New Command',
             'name' => isset($_POST['command']['name']) ? $_POST['command']['name'] : ifseta($_GET, 'name'),
             'url' => isset($_POST['command']['url']) ? $_POST['command']['url'] : null,
             'description' => isset($_POST['command']['description']) ? $_POST['command']['description'] : null,
-            'captchaHtml' => recaptcha_get_html(self::RECAPTCHA_PUBLIC_KEY),
+            'captchaHtml' => '<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+                              <div class="g-recaptcha" data-sitekey="' . self::RECAPTCHA_PUBLIC_KEY . '"></div>',
             'errorMessage' => $errorMessage,
         ));
     }
@@ -47,7 +47,6 @@ class CommandController extends Controller {
      * Processes the form for creating a new command.
      */
     public function action_add_command() {
-        require_once SERVER_ROOT . '/lib/Recaptcha/recaptchalib.php';
         $url = $_POST['command']['url'];
         $commandStore = new CommandStore($this->pdoSingleton->getPdo());
         $commandService = new CommandService();
@@ -73,12 +72,11 @@ class CommandController extends Controller {
             $this->redirectTo('/');
             return;
         }
-        $recaptchaResult = recaptcha_check_answer(self::RECAPTCHA_PRIVATE_KEY,
+        $recaptchaMatches = $this->recaptchaMatches(self::RECAPTCHA_PRIVATE_KEY,
                                 $_SERVER['REMOTE_ADDR'],
-                                $_POST["recaptcha_challenge_field"],
-                                $_POST["recaptcha_response_field"]);
-        if (!$recaptchaResult->is_valid) {
-            $this->forwardTo('new', array('The reCAPTCHA wasn’t entered correctly. Please try again.'));
+                                $_POST['g-recaptcha-response']);
+        if (!$recaptchaMatches) {
+            $this->forwardTo('new', array('Please click "I am not a robot."'));
             return;
         }
         $command = new Command();
@@ -91,4 +89,39 @@ class CommandController extends Controller {
         $this->redirectTo('/');
     }
 
+    /**
+     * Returns whether the captcha matches.
+     *
+     * @param string $privateKey The ReCaptcha private key.
+     * @param string $ipAddress  The user's IP address.
+     * @param string $recaptchaResponse The value of g-recaptcha-response.
+     */
+    protected function recaptchaMatches($privateKey, $ipAddress, $recaptchaResponse) {
+        $result = $this->post('https://www.google.com/recaptcha/api/siteverify', array(
+            'secret' => $privateKey,
+            'response' => $recaptchaResponse,
+            'remoteip' => $ipAddress
+        ));
+        $result = json_decode($result, true);
+        return $result['success'];
+    }
+
+    /**
+     * Posts to the given URL.
+     *
+     * @param string $url  The URL to post to.
+     * @param array  $data The key-value pairs to post.
+     */
+    protected function post ($url, $data) {
+        // use key 'http' even if you send the request to https://...
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($data),
+            ),
+        );
+        $context  = stream_context_create($options);
+        return file_get_contents($url, false, $context);
+    }
 }
