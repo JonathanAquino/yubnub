801b5b2191d46a33ad87758775b69c17165a317c
Jonathan Aquino <jonathan.aquino@gmail.com>
2020-06-01 15:05:21 -0700

    Revert "Do not execute command if command has args but command string does not."
    This reverts commit f89a5b9302115b0b9ebc11dc88ded01b3b4a37d2.
    
    It was causing problems with ls, create, etc.

--- app/helpers/Parser.php
+++ app/helpers/Parser.php
@@ -102,9 +102,6 @@ class Parser {
         if ($command && !$command->hasArgs() && $args) {
             $command = null;
         }
-        if ($command && $command->hasArgs() && !$args) {
-            $command = null;
-        }        
         if (!$command && !$defaultCommand) {
             throw new Exception('Could not find command ' . $name);
         }
